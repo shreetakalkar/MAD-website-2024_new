70fcf0a7c146d0dce47698443a4ce24377850053
Ritojnan <ritojnanm@gmail.com>
2024-06-26 18:30:09 +0530

    tailwind config update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,27 +70,7 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-    },
-    //className="2xl:w-[xyz] lg:w-[xyz]"
-    screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-  
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-  
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-  
-      md: { max: "767px" },
-      // => @media (max-width: 767px) { ... }
-  
-      sm: { max: "639px" },
-      // => @media (max-width: 639px) { ... }
-  
-      xs: { max: "479px" },
-      // => @media (max-width: 479px) { ... }
-  },
+    }
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
