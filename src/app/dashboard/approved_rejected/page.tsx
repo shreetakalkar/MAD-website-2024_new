345a0fb586b4059e66efe1609ca72f38f1f683c9
Mayuresh Sama <mayureshc007@gmail.com>
2024-06-29 00:10:20 +0530

    Implements Approve_Rejected page

---
+++ src/app/dashboard/approved_rejected/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Approved_Rejected from '@/pages/Approved_Rejected'
+import React from 'react'
+
+const approvedRejected = () => {
+  return (
+    <div>
+      <Approved_Rejected />
+    </div>
+  )
+}
+
+export default approvedRejected
