645fffc7b0f94afdfa2c7804f906e49a92953303
juhid30 <55.juhideore@gmail.com>
2024-06-29 22:40:47 +0530

    Update card filter

--- src/components/RailwayEntryInterface.tsx
+++ src/components/RailwayEntryInterface.tsx
@@ -300,7 +300,11 @@ const RailwayEntryInterface = () => {
         <CardContent>
           {" "}
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              method="post"
+              className="space-y-8"
+            >
               <div className="grid gap-4">
                 <div className="grid grid-cols-2 gap-4">
                   <div className="grid gap-2">
