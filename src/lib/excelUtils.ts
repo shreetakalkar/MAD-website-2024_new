8138d784cfe1433eb23981e949a812e1366be280
Anish Awasthi <anishawasthi16@gmail.com>
2024-07-08 00:11:21 +0530

    formatting excel sheet

--- src/lib/excelUtils.ts
+++ src/lib/excelUtils.ts
@@ -9,16 +9,16 @@ export const createExcelFile = async (enquiries: any[]): Promise<Blob | null> =>
 
     // Define the columns for both worksheets
     const columns = [
-      { header: "SRNO", key: "srno", width: 3.86 },
-      { header: "PASSNO", key: "passNum", width: 10.57 },
-      { header: "NAME", key: "name", width: 26.43 },
-      { header: "M/F", key: "gender", width: 2.57 },
-      { header: "DOB", key: "dob", width: 9.57 },
-      { header: "FROM", key: "from", width: 10.86 },
-      { header: "TO", key: "to", width: 10.86 },
-      { header: "CLASS I/II", key: "class", width: 6 },
-      { header: "MODE M/Q", key: "mode", width: 6 },
-      { header: "DATE OF ISSUE", key: "lastPassIssued", width: 9.57 },
+      { header: "Sr no", key: "srno", width: 3.86 },
+      { header: "Certificate number", key: "passNum", width: 10.57 },
+      { header: "Name", key: "name", width: 26.43 },
+      { header: "M/F", key: "gender", width: 2.67 }, //2.57
+      { header: "Date of Birth", key: "dob", width: 9.57 },
+      { header: "From", key: "from", width: 10.86 },
+      { header: "To", key: "to", width: 10.86 },
+      { header: "Class I/II", key: "class", width: 6 },
+      { header: "Mode M / Q", key: "mode", width: 6.17 }, //6
+      { header: "Date of Issue", key: "lastPassIssued", width: 9.57 },
       { header: "Address", key: "address", width: 38.57 },
     ];
 
@@ -36,32 +36,60 @@ export const createExcelFile = async (enquiries: any[]): Promise<Blob | null> =>
         header: 0.0,
         footer: 0.0,
       };
+
+      // Title row
       worksheet.mergeCells('A1:K1');
       worksheet.getCell('A1').value = 'Thadomal Shahani Engineering College';
       worksheet.getCell('A1').alignment = { vertical: 'middle', horizontal: 'center' };
       worksheet.getRow(1).height = 30;
       worksheet.getCell('A1').font = { size: 20, bold: true };
+
+      // Subheading row
+      worksheet.mergeCells('A2:K2');
+      worksheet.getCell('A2').alignment = { vertical: 'middle', horizontal: 'center' };
+      worksheet.getRow(2).height = 20;
+      worksheet.getCell('A2').font = { size: 14 };
+
       worksheet.columns.forEach((column) => {
-        column.style = { font: { size: 12 } };  // Set default font size for all columns
+        column.style = { font: { size: 12 }};  // Set default font size and alignment for all columns
       });
+
+      worksheet.addRow(columns.map(col => col.header));
+      // Apply wrapText only to the header row
+      worksheet.getRow(3).eachCell((cell) => {
+        cell.alignment = { wrapText: true, horizontal: 'center', };
+      });
+      worksheet.getRow(3).commit();
+      worksheet.getRow(3).height = 45; //self 
     });
+
+    westernWorksheet.getCell('A2').value = 'Railway Concessions for Western Railway';
+    centralWorksheet.getCell('A2').value = 'Railway Concessions for Central Railway';
 
     // Add the data rows to the appropriate worksheets
     let harborIndex = 1;
     let centralIndex = 1;
 
     enquiries.forEach((enquiry, index) => {
+      const formatDate = (date: Date) => {
+        const d = new Date(date);
+        const year = d.getFullYear().toString().substr(-2); // Get last 2 digits of the year
+        const month = String(d.getMonth() + 1).padStart(2, '0');
+        const day = String(d.getDate()).padStart(2, '0');
+        return `${day}/${month}/${year}`;
+      };
+
       const rowData = {
         srno: index + 1,
         passNum: enquiry.passNum,
         name: `${enquiry.lastName} ${enquiry.firstName} ${enquiry.middleName}`,
-        gender: enquiry.gender === 'Male' ? "M" : "F",
-        dob: enquiry.dob.toDate().toLocaleDateString(),
+        gender: enquiry.gender === 'Male' ? 'M' : 'F',
+        dob: formatDate(enquiry.dob.toDate()),
         from: enquiry.from,
         to: enquiry.to,
         class: enquiry.class,
-        mode: enquiry.duration,
-        lastPassIssued: enquiry.lastPassIssued?.toDate().toLocaleDateString() ?? "",
+        mode: enquiry.duration === 'Monthly' ? 'Mly' : 'Qty',
+        lastPassIssued: enquiry.lastPassIssued ? formatDate(enquiry.lastPassIssued.toDate()) : "",
         address: enquiry.address,
       };
 
@@ -69,8 +97,7 @@ export const createExcelFile = async (enquiries: any[]): Promise<Blob | null> =>
       if (travelLane === 'harbor' || travelLane === 'western' || travelLane === 'harbour') {
         westernWorksheet.addRow(rowData);
         harborIndex++;
-      }
-      else {
+      } else {
         centralWorksheet.addRow(rowData);
         centralIndex++;
       }
