0bfb869951e59604e12886992a49888b44992f2e
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-07-07 21:51:03 +0530

    Collected page sorted acc pass issued, year instead of gradyear

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,12 +3,13 @@ import RailwayUpdateConc from "@/pages/RailwayUpdateConc";
 import PendingRequests from "@/pages/PendingRequests";
 import CollectedPassTable from "@/components/Pass Collection/CollectedPass";
 import RailwayEntryInterface from "@/pages/RailwayEntryInterface";
+
 const pendingRequests = () => {
   return (
     <div className="h-[100vh]">
-      {/* <CollectedPassTable /> */}
+      <CollectedPassTable />
       {/* <RailwayUpdateConc /> */}
-      <RailwayEntryInterface />
+      {/* <RailwayEntryInterface /> */}
       {/* <PendingRequests /> */}
       {/* <RailwayUpdateCard/> */}
     </div>
