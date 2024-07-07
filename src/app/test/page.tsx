a5c2a8fce74dd7814d1cc4a7aa77e44aa273b11a
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-07-07 11:31:22 +0530

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,20 +2,12 @@
 import RailwayEntryInterface from "@/components/RailwayEntryInterface";
 import RailwayUpdateConc from "@/pages/RailwayUpdateConc";
 import PendingRequests from "@/pages/PendingRequests";
-<<<<<<< Updated upstream
 import CollectedPassTable from "@/components/Pass Collection/CollectedPass";
 const pendingRequests = () => {
   return (
     <div className="h-[100vh]">
       <CollectedPassTable />
       {/* <RailwayUpdateConc /> */}
-=======
-import RailwayUpdateCard from "@/pages/RailwayUpdateConc";
-
-const pendingRequests = () => {
-  return (
-    <div>
->>>>>>> Stashed changes
       {/* <RailwayEntryInterface /> */}
       {/* <PendingRequests /> */}
       <RailwayUpdateCard/>
