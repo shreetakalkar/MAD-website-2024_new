e5209d17068e4b11c85f49bd79adf6628cbc1d0f
Anish Awasthi <anishawasthi16@gmail.com>
2024-07-28 21:31:50 +0530

    zod resolved for the exam dept form

--- src/components/ExamDept.tsx
+++ src/components/ExamDept.tsx
@@ -1,38 +1,50 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import TTForm from "./TTForm";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
 
 import { db, storage } from "@/config/firebase";
-import {
-  getFirestore,
-  doc,
-  updateDoc,
-  arrayUnion,
-  Timestamp,
-} from "firebase/firestore";
+import { doc, updateDoc, arrayUnion, Timestamp } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import ClipLoader from "react-spinners/ClipLoader";
 import useGradYear from "@/constants/gradYearList";
+import { ClipLoader } from "react-spinners";
+
+// Define the schema for form validation
+const formSchema = z.object({
+  title: z.string().nullable(),
+  description: z.string().nullable(),
+  year: z.string().nullable(),
+  branch: z.string().nullable(),
+  division: z.string().nullable(),
+  batch: z.string().nullable(),
+  startDate: z.date().nullable().optional(),
+  endDate: z.date().nullable().optional(),
+  file: z.instanceof(File).optional().nullable(),
+});
+
+type FormData = z.infer<typeof formSchema>;
 
 const ExamDept = () => {
-  const methods = useForm();
   const { toast } = useToast();
   const gradYearList = useGradYear();
-
-  const [currentYear, setCurrentYear] = useState<string>("All");
-  const [branch, setBranch] = useState<string>("All");
-  const [div, setDivision] = useState<string>("All");
-  const [batch, setBatch] = useState<string>("All");
-  const [title, setTitle] = useState<string>("");
-  const [content, setContent] = useState<string>("");
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
-  const [docURL, setdocURL] = useState<string | null>(null);
-  const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+
+  const methods = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      year: "All",
+      branch: "All",
+      division: "All",
+      batch: "All",
+    },
+  });
+
   const additionalFields = [
     {
       name: "startDate",
@@ -54,8 +66,7 @@ const ExamDept = () => {
     },
   ];
 
-  const handleUpload = async (data) => {
-    let uploadeddocURL = docURL;
+  const handleUpload = async (data: FormData) => {
     const {
       title,
       description,
@@ -67,132 +78,127 @@ const ExamDept = () => {
       endDate,
       file,
     } = data;
-    const currentYear = year;
+
+    let uploadedDocURL = null;
+
     if (file) {
-      setLoading(true);
       const storageRef = ref(
         storage,
         `ImportantNotice/ExamTimeTable/${file.name}`
       );
       const uploadTask = uploadBytesResumable(storageRef, file);
 
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Handle file upload progress here if needed
-        },
-        (error) => {
-          console.error("Error uploading file: ", error);
-          setLoading(false);
-        },
-        async () => {
-          uploadeddocURL = await getDownloadURL(uploadTask.snapshot.ref);
-          setdocURL(uploadeddocURL);
-          setLoading(false);
-
-          // After getting the file URL, proceed with Firestore update
-          let selectedGradYear;
-          if (currentYear != "All") {
-            selectedGradYear = gradYearList.find(
-              (item) => item.year === currentYear
-            )?.gradYear;
-          } else {
-            selectedGradYear = "All";
-          }
-
-          const uploadData = {
-            title,
-            content: description,
-            gradYear: selectedGradYear,
-            branch: branch || "All",
-            div: division || "All",
-            batch: batch || "All",
-            startDate: startDate ? Timestamp.fromDate(startDate) : null,
-            endDate: endDate ? Timestamp.fromDate(endDate) : null,
-            docURL: uploadeddocURL,
-          };
-          try {
-            const docRef = doc(db, "ImportantNotice", "Content");
-            await updateDoc(docRef, {
-              content: arrayUnion(uploadData),
-            });
-            methods.reset(); // Reset the form fields
-            console.log("Data successfully added!");
-          } catch (error) {
-            console.error("Error adding data: ", error);
-          }
-        }
-      );
-    } else {
-      // If no file was uploaded, proceed with just the other form data
-      const uploadData = {
-        title,
-        content: description,
-        gradYear: currentYear,
-        branch: branch || "All",
-        div: division || "All",
-        batch: batch || "All",
-        startDate: startDate ? Timestamp.fromDate(startDate) : null,
-        endDate: endDate ? Timestamp.fromDate(endDate) : null,
-        docURL,
-      };
       try {
-        const docRef = doc(db, "ImportantNotice", "Content");
-        await updateDoc(docRef, {
-          content: arrayUnion(uploadData),
-        });
-        methods.reset(); // Reset the form fields
-        console.log("Data successfully added!");
+        await uploadTask;
+        uploadedDocURL = await getDownloadURL(uploadTask.snapshot.ref);
       } catch (error) {
-        console.error("Error adding data: ", error);
+        console.error("Error uploading file: ", error);
+        toast({
+          description: "Error uploading file. Please try again.",
+          variant: "destructive",
+        });
+        return;
       }
+    }
+
+    const selectedGradYear =
+      year !== "All"
+        ? gradYearList.find((item) => item.year === year)?.gradYear || "All"
+        : "All";
+
+    const uploadData = {
+      title,
+      content: description,
+      gradYear: selectedGradYear,
+      branch: branch || "All",
+      div: division || "All",
+      batch: batch || "All",
+      startDate: startDate ? Timestamp.fromDate(startDate) : null,
+      endDate: endDate ? Timestamp.fromDate(endDate) : null,
+      docURL: uploadedDocURL,
+    };
+
+    try {
+      const docRef = doc(db, "ImportantNotice", "Content");
+      await updateDoc(docRef, {
+        content: arrayUnion(uploadData),
+      });
+
+      toast({
+        description: "Data successfully added!",
+        variant: "default",
+      });
+    } catch (error) {
+      console.error("Error adding data: ", error);
+      toast({
+        description: "Error adding data. Please try again.",
+        variant: "destructive",
+      });
     }
   };
 
-  const onSubmit = async (data) => {
-    const missingFields = Object.keys(data).filter(
-      (key) =>
-        !data[key] || (Array.isArray(data[key]) && data[key].length === 0)
-    );
+  useEffect(() => {
+    console.log(loading);
+  }, [loading]);
 
-    if (missingFields.length > 0) {
-      toast({
-        description: "Please fill in all required fields.",
-        variant: "destructive",
-      });
-      return;
+  const onSubmit = (data: FormData) => {
+    setLoading(true);
+
+    console.log(data);
+    let flag = true;
+    Object.entries(data).forEach(([_, value]) => {
+      if (value === "" || value === null) {
+        toast({
+          description: "Please fill all required fields.",
+          variant: "destructive",
+        });
+        flag = false;
+      }
+    });
+    if (flag) {
+      handleUpload(data);
+      methods.reset();
     }
-
-    handleUpload(data);
+    setLoading(false);
   };
 
   return (
-    <div className="w-[100%] flex justify-center items-center">
-      <FormProvider {...methods}>
-        <Card
-          style={{
-            overflow: "auto",
-            WebkitOverflowScrolling: "touch",
-            scrollbarWidth: "none",
-            msOverflowStyle: "none",
-          }}
-          className="w-[95%] no-scrollbar"
-        >
-          <CardHeader>
-            <CardTitle className="text-3xl">Exam Department Form</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <TTForm
-              additionalFields={additionalFields}
-              onSubmit={onSubmit}
-              handleSubmit={methods.handleSubmit}
-              control={methods.control}
-              reset={methods.reset}
-            />
-          </CardContent>
-        </Card>
-      </FormProvider>
-    </div>
+    <>
+      <div className="w-[100%] flex justify-center items-center">
+        {loading && (
+          <div className="flex items-center justify-center h-screen">
+            <ClipLoader size={50} color={"#123abc"} loading={loading} />
+            {/* <p>Loading...</p> */}
+          </div>
+        )}
+
+        {!loading && (
+          <FormProvider {...methods}>
+            <Card
+              style={{
+                overflow: "auto",
+                WebkitOverflowScrolling: "touch",
+                scrollbarWidth: "none",
+                msOverflowStyle: "none",
+              }}
+              className="w-[95%] no-scrollbar"
+            >
+              <CardHeader>
+                <CardTitle className="text-3xl">Exam Department Form</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <TTForm
+                  additionalFields={additionalFields}
+                  handleSubmit={methods.handleSubmit(onSubmit)}
+                  control={methods.control}
+                  reset={methods.reset}
+                />
+              </CardContent>
+            </Card>
+          </FormProvider>
+        )}
+      </div>
+    </>
   );
 };
 
