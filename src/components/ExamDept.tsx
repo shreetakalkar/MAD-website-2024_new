0157782537c01a957fe75b04b73cc675a390ea08
juhid30 <55.juhideore@gmail.com>
2024-07-28 21:53:03 +0530

    import

--- src/components/ExamDept.tsx
+++ src/components/ExamDept.tsx
@@ -4,15 +4,8 @@ import TTForm from "./TTForm";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/components/ui/use-toast";
-
 import { db, storage } from "@/config/firebase";
-import {
-  getFirestore,
-  doc,
-  updateDoc,
-  arrayUnion,
-  Timestamp,
-} from "firebase/firestore";
+import { doc, updateDoc, arrayUnion, Timestamp } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import ClipLoader from "react-spinners/ClipLoader";
 import useGradYear from "@/constants/gradYearList";
@@ -24,15 +17,11 @@ const ExamDept = () => {
 
   const [currentYear, setCurrentYear] = useState<string>("All");
   const [branch, setBranch] = useState<string>("All");
-  const [div, setDivision] = useState<string>("All");
+  const [division, setDivision] = useState<string>("All");
   const [batch, setBatch] = useState<string>("All");
-  const [title, setTitle] = useState<string>("");
-  const [content, setContent] = useState<string>("");
-  const [startDate, setStartDate] = useState<Date | null>(null);
-  const [endDate, setEndDate] = useState<Date | null>(null);
-  const [docURL, setdocURL] = useState<string | null>(null);
-  const [file, setFile] = useState<File | null>(null);
+  const [docURL, setDocURL] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+
   const additionalFields = [
     {
       name: "startDate",
@@ -54,8 +43,9 @@ const ExamDept = () => {
     },
   ];
 
-  const handleUpload = async (data) => {
-    let uploadeddocURL = docURL;
+  const handleUpload = async (data: any) => {
+    setLoading(true);
+
     const {
       title,
       description,
@@ -67,89 +57,74 @@ const ExamDept = () => {
       endDate,
       file,
     } = data;
+
+    let uploadeddocURL = docURL;
     const currentYear = year;
+
     if (file) {
-      setLoading(true);
-      const storageRef = ref(
-        storage,
-        `ImportantNotice/ExamTimeTable/${file.name}`
-      );
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Handle file upload progress here if needed
-        },
-        (error) => {
-          console.error("Error uploading file: ", error);
-          setLoading(false);
-        },
-        async () => {
-          uploadeddocURL = await getDownloadURL(uploadTask.snapshot.ref);
-          setdocURL(uploadeddocURL);
-          setLoading(false);
-
-          // After getting the file URL, proceed with Firestore update
-          let selectedGradYear;
-          if (currentYear != "All") {
-            selectedGradYear = gradYearList.find(
-              (item) => item.year === currentYear
-            )?.gradYear;
-          } else {
-            selectedGradYear = "All";
-          }
-
-          const uploadData = {
-            title,
-            content: description,
-            gradYear: selectedGradYear,
-            branch: branch || "All",
-            div: division || "All",
-            batch: batch || "All",
-            startDate: startDate ? Timestamp.fromDate(startDate) : null,
-            endDate: endDate ? Timestamp.fromDate(endDate) : null,
-            docURL: uploadeddocURL,
-          };
-          try {
-            const docRef = doc(db, "ImportantNotice", "Content");
-            await updateDoc(docRef, {
-              content: arrayUnion(uploadData),
-            });
-            methods.reset(); // Reset the form fields
-            console.log("Data successfully added!");
-          } catch (error) {
-            console.error("Error adding data: ", error);
-          }
-        }
-      );
-    } else {
-      // If no file was uploaded, proceed with just the other form data
-      const uploadData = {
-        title,
-        content: description,
-        gradYear: currentYear,
-        branch: branch || "All",
-        div: division || "All",
-        batch: batch || "All",
-        startDate: startDate ? Timestamp.fromDate(startDate) : null,
-        endDate: endDate ? Timestamp.fromDate(endDate) : null,
-        docURL,
-      };
       try {
-        const docRef = doc(db, "ImportantNotice", "Content");
-        await updateDoc(docRef, {
-          content: arrayUnion(uploadData),
-        });
-        methods.reset(); // Reset the form fields
-        console.log("Data successfully added!");
+        const storageRef = ref(
+          storage,
+          `ImportantNotice/ExamTimeTable/${file.name}`
+        );
+        const uploadTask = await uploadBytesResumable(storageRef, file);
+
+        uploadeddocURL = await getDownloadURL(uploadTask.snapshot.ref);
+        setDocURL(uploadeddocURL);
       } catch (error) {
-        console.error("Error adding data: ", error);
+        console.error("Error uploading file: ", error);
+        toast({
+          description: "Error uploading file.",
+          variant: "destructive",
+        });
+        setLoading(false);
+        return;
       }
+    }
+
+    let selectedGradYear;
+    if (currentYear !== "All") {
+      selectedGradYear = gradYearList.find(
+        (item) => item.year === currentYear
+      )?.gradYear;
+    } else {
+      selectedGradYear = "All";
+    }
+
+    const uploadData = {
+      title,
+      content: description,
+      gradYear: selectedGradYear,
+      branch: branch || "All",
+      div: division || "All",
+      batch: batch || "All",
+      startDate: startDate ? Timestamp.fromDate(startDate) : null,
+      endDate: endDate ? Timestamp.fromDate(endDate) : null,
+      docURL: uploadeddocURL,
+    };
+
+    try {
+      const docRef = doc(db, "ImportantNotice", "Content");
+      await updateDoc(docRef, {
+        content: arrayUnion(uploadData),
+      });
+      methods.reset();
+      toast({
+        description: "Data successfully added!",
+        variant: "success",
+      });
+    } catch (error) {
+      console.error("Error adding data: ", error);
+      toast({
+        description: "Error adding data.",
+        variant: "destructive",
+      });
+    } finally {
+      setLoading(false);
     }
   };
 
-  const onSubmit = async (data) => {
+  const onSubmit = (data: any) => {
     const missingFields = Object.keys(data).filter(
       (key) =>
         !data[key] || (Array.isArray(data[key]) && data[key].length === 0)
@@ -183,11 +158,20 @@ const ExamDept = () => {
           </CardHeader>
           <CardContent>
             <TTForm
+              currentYear={currentYear}
+              setCurrentYear={setCurrentYear}
+              branch={branch}
+              setBranch={setBranch}
+              division={division}
+              setDivision={setDivision}
+              batch={batch}
+              setBatch={setBatch}
               additionalFields={additionalFields}
               onSubmit={onSubmit}
               handleSubmit={methods.handleSubmit}
               control={methods.control}
               reset={methods.reset}
+              loading={loading}
             />
           </CardContent>
         </Card>
