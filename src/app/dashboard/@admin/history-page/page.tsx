94d43a19f6814b9d97a4dba3d5fc1350d2411c4a
Mayuresh Sama <mayureshc007@gmail.com>
2024-08-19 00:18:19 +0530

    Initialises Admin History and Committe History pages & ADDS EAStTER-EGG in ADMIN page

---
+++ src/app/dashboard/@admin/history-page/page.tsx
@@ -0,0 +1,12 @@
+import AdminHistory from '@/components/AdminHistory'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <AdminHistory />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/AdminHistory.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminHistory = () => {
+  return (
+    <div>Will do it tommrrow</div>
+  )
+}
+
+export default AdminHistory
