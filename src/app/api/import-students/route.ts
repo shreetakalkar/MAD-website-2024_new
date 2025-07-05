9f940724e675c4877fcdfb3e0248a93125f04452
Shree Takalkar <shreetakalkar087@gmail.com>
2025-07-05 14:33:03 +0530

    utilites integration

--- src/app/api/import-students/route.ts
+++ src/app/api/import-students/route.ts
@@ -11,7 +11,7 @@ if (!getApps().length) {
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        privateKey: process.env.IREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       }),
     })
   } catch (error) {
